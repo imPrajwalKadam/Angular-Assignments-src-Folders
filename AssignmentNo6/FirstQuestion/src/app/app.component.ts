4fd9960cf3bc6ab9a3d2c3b50b9f97ce784b8f95
Prajwal Kadam <81584701+imPrajwalKadam@users.noreply.github.com>
2021-08-18 16:04:47 +0530

    Delete AssignmentNo6 directory

--- AssignmentNo6/FirstQuestion/src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  template: '<html><label for="fname">Marvellous Infosystems:</label><input type="text" id="fname" name="fname"></html>'
-})
-export class AppComponent {
-  title = 'FirstQuestion';
-}
