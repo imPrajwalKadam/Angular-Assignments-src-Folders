78ade9e8cf789909d55f00ae8de506a5acf0eeba
Prajwal Kadam <81584701+imPrajwalKadam@users.noreply.github.com>
2021-08-18 16:06:36 +0530

    Add files via upload

---
+++ FirstngQuestionAssignmentNo6/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<html><label for="fname">Marvellous Infosystems:</label><input type="text" id="fname" name="fname"></html>'
+})
+export class AppComponent {
+  title = 'FirstQuestion';
+}
